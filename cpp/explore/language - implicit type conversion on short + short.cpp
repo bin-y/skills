a407087aad0458a01de8f719e216bd6669d5da3f
bin-y <wye.mia.kid.c@gmail.com>
2019-03-07 02:23:56 +0800

    cpp/explore: language - implicit type conversion on short + short

---
+++ cpp/explore/language - implicit type conversion on short + short.cpp
@@ -0,0 +1,10 @@
+﻿#include <cstdint>
+#include <type_traits>
+
+void main() {
+    uint16_t a = 1;
+    auto b = a + (uint16_t)1;
+    // static assertion failed with "hmmm..."
+    static_assert(std::is_same<decltype(b), decltype(a)>::value, "hmmm...");
+    // see: https://stackoverflow.com/a/5563131/4533234
+}
